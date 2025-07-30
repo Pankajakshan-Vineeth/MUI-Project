cabc81f5accfb1d7535cdcb14381b27d3dd302be
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-07-30 19:30:24 +0530

    first project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import '@fontsource/roboto/300.css';
+import Button from '@mui/material/Button';
+
+
+
+const App = () => {
+  return (
+    <div>
+ <Button variant="contained">Hello world</Button>;
+    </div>
+  )
+}
+
+export default App
