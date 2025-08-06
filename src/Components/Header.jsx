6bcd632c32943eaead5af620b68455a35435cfe1
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-08-06 19:05:07 +0530

    header compledted and md wip

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import CustomButton from "./CustomBotton/CustomButton";
+import logoImg from "../assets/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
+import HomeIcon from "@mui/icons-material/Home";
+import PhoneIcon from "@mui/icons-material/Phone";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState({});
+
+  const slideMenu = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setMobileMenu({ ...mobileMenu, [anchor]: open });
+  };
+
   const nav_headings = [
     {
       path: "/",
@@ -43,21 +69,79 @@ const Header = () => {
     },
   }));
 
+  const NavBarlogo = styled("img")(({ theme }) => ({
+    cursor: "pointer",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
+
+  const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
+    cursor: "pointer",
+    display: "none",
+    [theme.breakpoints.down("md")]: {
+      display: "block",
+    },
+  }));
+
   return (
-    <Box>
-      <Box>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "40px",
+        maxWidth: "auto",
+        backgroundColor: "#FED801",
+        marginLeft: "0px",
+        marginBottom: "-24px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        <CustomMenuIcon onClick={slideMenu("left", true)} />
+        <Drawer
+          anchor="left"
+          open={mobileMenu["left"] || false}
+          onClose={slideMenu("left", false)}
+        />
+        <NavBarlogo src={logoImg} alt="" />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        ></Box>
         <NavBarLinksBox>
           {nav_headings.map((item, index) => (
-            <NavBarLink variant="body2">{item.display}</NavBarLink>
+            <NavBarLink key={index} variant="body2">
+              {item.display}
+            </NavBarLink>
           ))}
         </NavBarLinksBox>
       </Box>
-      <NavBarLink variant="body2">Sign Up</NavBarLink>
-      <CustomButton
-        backgroundColor="#0F184C"
-        color="#FFF"
-        buttonText="Register"
-      />
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        <NavBarLink variant="body2">Sign Up</NavBarLink>
+        <CustomButton
+          backgroundColor="#0F184C"
+          color="#FFF"
+          buttonText="Register"
+        />
+      </Box>
     </Box>
   );
 };
